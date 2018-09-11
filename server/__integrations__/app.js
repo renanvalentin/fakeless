3e068098cc88c94852ce10c90e23c16afb750272
Renan Ferreira <renan.ferreira@zalando.de>
2018-09-11 09:54:29 +0200

    feat(routes): match route with querystring

--- server/__integrations__/app.js
+++ server/__integrations__/app.js
@@ -195,3 +195,54 @@ it('validates body payload', async () => {
     })
     .expect(400);
 });
+
+it('filter router by query string', async () => {
+  const setup = {
+    variables: {
+      host: 'http://localhost:3000',
+      rootDir: '../../',
+    },
+    templates: [
+      {
+        variables: {
+          hero: 'spider',
+        },
+        method: 'get',
+        route: '/heroes/<%= hero %>',
+        query: {
+          power: 'true',
+        },
+        response: {
+          body: {
+            name: '<%= hero %>',
+            power: 'spider sense',
+          },
+          status: 200,
+        },
+      },
+      {
+        variables: {
+          hero: 'spider',
+        },
+        method: 'get',
+        route: '/heroes/<%= hero %>',
+        response: {
+          body: {
+            name: '<%= hero %>',
+            photos: '<%= host %>/<%= hero %>/photos',
+          },
+          status: 200,
+        },
+      },
+    ],
+  };
+
+  const app = await createApp(setup);
+
+  return supertest(app)
+    .get('/heroes/spider?power=true')
+    .expect((res) => {
+      expect(res.text).toMatchSnapshot();
+    })
+    .expect(200);
+});
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,38 +1,47 @@
 /* @flow */
 
-import { readFile } from 'fs';
-import { promisify } from 'util';
-import { join as joinPath } from 'path';
+import { readFile } from "fs";
+import { promisify } from "util";
+import { join as joinPath } from "path";
+import _ from "lodash";
 
-import { Router } from 'express';
+import { Router } from "express";
 
-import { resolveTemplate, log } from '../utils';
-import type { Setup, Template, Variables } from '../types';
+import { resolveTemplate, log } from "../utils";
+import type { Setup, Template, Variables } from "../types";
 
-const logger = log('routes');
+const logger = log("routes");
 
 const readFileAsync = promisify(readFile);
 
 type LoadResponseDef = (path: string) => Promise<{}>;
 
 const loadReponse: LoadResponseDef = path =>
-  readFileAsync(joinPath(process.cwd(), path), 'utf-8').then(str => JSON.parse(str));
+  readFileAsync(joinPath(process.cwd(), path), "utf-8").then(str =>
+    JSON.parse(str)
+  );
 
-type ParseTemplateDef = (template: Template, variables: Variables) => Promise<Template>;
+type ParseTemplateDef = (
+  template: Template,
+  variables: Variables
+) => Promise<Template>;
 
 const parseTemplate: ParseTemplateDef = (template, variables) =>
-  new Promise(async (res) => {
-    const resolved = resolveTemplate(template, { ...variables, ...template.variables });
+  new Promise(async res => {
+    const resolved = resolveTemplate(template, {
+      ...variables,
+      ...template.variables
+    });
 
-    if (typeof template.response.body === 'string') {
+    if (typeof template.response.body === "string") {
       const body = await loadReponse(resolved.response.body);
 
       return res({
         ...template,
         response: {
           ...template.response,
-          body,
-        },
+          body
+        }
       });
     }
 
@@ -41,19 +50,23 @@ const parseTemplate: ParseTemplateDef = (template, variables) =>
 
 type MakeRouteDef = (
   template: Template,
-  variables: Variables,
+  variables: Variables
 ) => Promise<{
   type: string,
   path: string,
   status: number,
-  response: { [key: string]: string },
+  query: {},
+  response: { [key: string]: string }
 }>;
 
 const makeRoute: MakeRouteDef = (template, variables) =>
-  new Promise(async (res) => {
+  new Promise(async res => {
     const parsedTemplate = await parseTemplate(template, variables);
 
-    const resolved = resolveTemplate(parsedTemplate, { ...variables, ...template.variables });
+    const resolved = resolveTemplate(parsedTemplate, {
+      ...variables,
+      ...template.variables
+    });
 
     const routeType = resolved.method.toLowerCase();
 
@@ -61,22 +74,58 @@ const makeRoute: MakeRouteDef = (template, variables) =>
       type: routeType,
       path: resolved.route,
       status: resolved.response.status,
-      response: resolved.response.body,
+      query: resolved.query || {},
+      response: resolved.response.body
     });
+  });
+
+const findRouteForRequest = (routes, req) =>
+  routes.find(route => {
+    return _.isEqual(req.query, route.query);
+  });
+
+const defineRoute = router => ({ type, name, routes }) =>
+  // $FlowFixMe
+  router[type](name, (req, res) => {
+    const matchedRoute = findRouteForRequest(routes, req);
+
+    res.status(matchedRoute.status).send(matchedRoute.response);
+  });
+
+const arrangeRoutes = (routes, iteratee) =>
+  _.forEach(_.groupBy(routes, "type"), (groupedRoutesByType, type) => {
+    _.forEach(
+      _.groupBy(groupedRoutesByType, "path"),
+      (groupedRoutesByPath, name) => {
+        iteratee({
+          type,
+          name,
+          routes: groupedRoutesByPath
+        });
+      }
+    );
   });
 
 export const create = async (setup: Setup) => {
   const router = new Router();
 
-  const queue = setup.templates.map(template => makeRoute(template, setup.variables));
+  const queue = setup.templates.map(template =>
+    makeRoute(template, setup.variables)
+  );
 
   const routes = await Promise.all(queue);
 
-  routes.forEach((route) => {
-    logger.debug('created', route);
+  const routeMaker = defineRoute(router);
+
+  arrangeRoutes(routes, ({ type, name, routes: arrangedRoutes }) => {
+    logger.debug("created", arrangedRoutes);
 
     // $FlowFixMe
-    router[route.type](route.path, (req, res) => res.status(route.status).send(route.response));
+    routeMaker({
+      type,
+      name,
+      routes: arrangedRoutes
+    });
   });
 
   return router;
